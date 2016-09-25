fa856ab07eabf7b7fa3bee1529c76d7ade2bdaf9
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-09-24 22:54:54 -0700

    Naming fixes

--- Assets/Physics/PhysicsTools.cs
+++ Assets/Physics/PhysicsTools.cs
@@ -7,16 +7,26 @@ public class PhysicsTools {
 	public 	static void Explode (Vector3 position, float radius, float force, float liftMod, List<GameObject> toIgnore = null) {
 		Action<Rigidbody> explode = (Rigidbody body) => body.AddExplosionForce (force, position, radius, liftMod);
 
-		ApplyInArea (explode, position, radius, toIgnore);
+		ApplyInVolume (explode, position, radius, toIgnore);
 	}
 
 	public static void ApplyForceInVolume (Vector3 position, float radius, Vector3 force, List<GameObject> toIgnore = null) {
 		Action<Rigidbody> applyForce = (Rigidbody body) => body.AddForce (force);
 
-		ApplyInArea (applyForce, position, radius, toIgnore);
+		ApplyInVolume (applyForce, position, radius, toIgnore);
 	}
 
-	public static void ApplyInArea(Action<Rigidbody> effect, Vector3 position, float radius, List<GameObject> toIgnore) {
+	public static IEnumerator TornadoInVolume(Vector3 position, float radius, float force, float lift, float duration) {
+		throw new NotImplementedException ();
+		//TODO
+		float tillDone = duration;
+		while(tillDone > 0) {
+			
+			yield return 0;
+		}
+	}
+
+	public static void ApplyInVolume(Action<Rigidbody> effect, Vector3 position, float radius, List<GameObject> toIgnore) {
 		Collider[] colliders = Physics.OverlapSphere (position, radius);
 
 		foreach (Collider col in colliders) {
